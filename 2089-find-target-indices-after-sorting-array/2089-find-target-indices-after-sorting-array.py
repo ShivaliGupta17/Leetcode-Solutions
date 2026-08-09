e907c68308faab15ca007f55148b051e74ceddfd
Shivali Gupta <gptshiv94@gmail.com>
2026-08-10 01:52:56 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.03%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,33 +1,10 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
 
-        def mergeSort(nums):
-            if len(nums) <= 1:
-                return nums
-
-            mid = len(nums) // 2
-
-            left = mergeSort(nums[:mid])
-            right = mergeSort(nums[mid:])
-
-            result = []
-            i = j = 0
-
-            while i < len(left) and j < len(right):
-                if left[i] <= right[j]:
-                    result.append(left[i])
-                    i += 1
-                else:
-                    result.append(right[j])
-                    j += 1
-
-            result.extend(left[i:])
-            result.extend(right[j:])
-
-            return result
-        nums = mergeSort(nums)
+        result=[]
+        result = sorted(nums)
         l=[]
-        for i in range(len(nums)):
-            if nums[i]==target:
+        for i in range(len(result)):
+            if result[i]==target:
                 l.append(i)
         return l
