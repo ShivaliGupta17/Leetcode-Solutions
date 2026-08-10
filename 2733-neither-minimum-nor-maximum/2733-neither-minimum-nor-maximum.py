e41fa9a3178a6177a3b061dfe5ba6dab2cdb869f
Shivali Gupta <gptshiv94@gmail.com>
2026-08-11 01:27:35 +0530

    Time: 20 ms (12.78%), Space: 19.3 MB (52.56%) - LeetHub

---
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findNonMinOrMax(self, nums: List[int]) -> int:
+        nums.sort()
+        if len(set(nums))==len(nums) and len(nums)<=2:
+            return -1
+        for i in range(1,len(nums)-1):
+            if nums[i]!=nums[i+1] and nums[i]!=min(nums) and nums[i]!=max(nums):
+                return nums[i]
