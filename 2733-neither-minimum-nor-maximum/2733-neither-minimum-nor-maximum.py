fa1645a49da428134a64379043d2fa608a925639
Shivali Gupta <gptshiv94@gmail.com>
2026-08-11 01:38:27 +0530

    Time: 7 ms (73.20%), Space: 19.2 MB (52.56%) - LeetHub
    Fix logic to handle edge cases for non-min/max values.

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -3,6 +3,7 @@ class Solution:
         nums.sort()
         '''if len(set(nums))==len(nums) and len(nums)<=2:
             return -1'''
+        
         for i in range(1,len(nums)-1):
             if nums[i]!=nums[i+1] and nums[i]!=min(nums) and nums[i]!=max(nums):
                 return nums[i]
