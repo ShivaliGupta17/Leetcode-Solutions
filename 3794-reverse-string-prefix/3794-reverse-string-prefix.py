5f00cae12be25b0e346f708ee86835f6078da6be
Shivali Gupta <gptshiv94@gmail.com>
2026-08-20 23:34:12 +0530

    Time: 0 ms (100%), Space: 19.3 MB (52.69%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.py
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.py
@@ -7,5 +7,5 @@ class Solution:
             s[left],s[right]=s[right],s[left]
             left+=1
             right-=1
-        return "".join(s[i] for i in range(len(s)))
+        return "".join(s)
         
