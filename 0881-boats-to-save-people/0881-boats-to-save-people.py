c1d692f0acca10f28357be1931da04bdff844a53
Shivali Gupta <gptshiv94@gmail.com>
2026-08-20 23:24:16 +0530

    Time: 46 ms (91.65%), Space: 25.4 MB (7.42%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,17 +1,13 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-
-        slow = 0
-        fast = len(people) - 1
-        count = 0
-
-        while slow <= fast:
-
-            if people[slow] + people[fast] <= limit:
-                slow += 1
-
-            fast -= 1
-            count += 1
+        slow=0
+        fast=len(people)-1
+        count=0
+        while slow<=fast:
+            if people[slow]+people[fast]<=limit:
+                slow+=1
+            fast-=1
+            count+=1
 
         return count
