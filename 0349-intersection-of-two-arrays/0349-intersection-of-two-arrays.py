57e3e3ea041e00b5a8943f45a4668ac9e55d80ab
Shivali Gupta <gptshiv94@gmail.com>
2026-09-09 00:23:58 +0530

    Time: 1 ms (40.5%), Space: 19.4 MB (45.29%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,4 +1,4 @@
-class Solution:
+'''class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         result=[]
         if len(nums1)>len(nums2):
@@ -9,4 +9,14 @@ class Solution:
             for i in range(len(nums2)):
                 if nums2[i] in nums1:
                     result.append(nums2[i])
+        return list(set(result))'''
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums2 = set(nums2)
+        result = []
+
+        for x in nums1:
+            if x in nums2:
+                result.append(x)
+
         return list(set(result))
